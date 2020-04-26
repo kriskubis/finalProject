0fde1188a5fcd8dcbe96a541e7e5428d81f158af
Mai Even <eaammel@students.eaaa.dk>
2020-04-26 12:53:33 +0200

    Dynamic admin page
    Added functions to change edit options

---
+++ tiamoto/js/admin.js
@@ -0,0 +1,73 @@
+//----------------------- EDIT EXISTING ACTIVITY -----------------------//
+// Create json request "req"
+let req = new Request('json/activities.json');
+
+// Add existing activities to step 1
+fetch(req)
+.then(function(response) {
+  return response.json();
+  })
+
+  .then(json => {
+    appendOptions(json.activities);
+  });
+
+  function appendOptions(activities) {
+  for (let activity of activities) {
+    activityOptions.innerHTML += `
+    <input type="radio" name="actTitle" value="${activity.title}" onClick="getActValue()"> ${activity.title} <br>
+      `;
+  }
+}
+
+// Get value of the chosen activity in step 1 (var must be global)
+var chosenActivity = {};
+function getActValue() {
+    chosenActivity.value = event.target.value;
+}
+
+// Get value of the chosen field in step 2 (var must be global)
+var chosenField = {};
+function getFieldValue() {
+    chosenField.value = event.target.value;
+}
+
+// Show available options or...
+function priceOptions() {
+    document.getElementById("infoField").innerHTML = '<input type="radio" name="newInfo" value="$"> $ <br><input type="radio" name="newInfo" value="$$"> $$ <br><input type="radio" name="newInfo" value="$$$"> $$$ <br>';
+}
+function catOptions() {
+    document.getElementById("infoField").innerHTML = '<input type="radio" name="cat" value="dining"> Dining Out <br><input type="radio" name="cat" value="city"> Exploring the City <br><input type="radio" name="cat" value="outdoor"> Relaxed Outdoor Activities <br><input type="radio" name="cat" value="amusement"> Amusement <br><input type="radio" name="cat" value="learning"> Learning <br><input type="radio" name="cat" value="active"> Active <br>';
+}
+function indoorOptions() {
+    document.getElementById("infoField").innerHTML = '<input type="radio" name="indoors" value="true"> Indoor <br><input type="radio" name="indoors" value="false"> Outdoor <br>';
+}
+
+// ...show current content in step 3
+function getContent() {
+    
+    fetch(req)
+    .then(function(response) {
+        return response.json();
+    })
+      
+    .then(json => {
+        appendOptions(json.activities);
+    });
+
+    // SHOULD FILTER ACCORDING TO chosenActivity AND chosenField
+    console.log('Showing the content of "' + chosenField.value + '" for ' + chosenActivity.value);
+
+    function appendOptions(activities) {
+        for (let activity of activities) {
+            
+            //if (title.includes(chosenActivity)) {
+            //   console.log(activity.identifier)
+            //}
+            
+            infoField.innerHTML = `
+            <input type="text" name="newContent" size="50" maxlength="100" value="(${activity.intro})"><br>
+            `;
+        }
+    }   
+}
